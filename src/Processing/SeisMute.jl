4d95bf33664cefe603bf3b3553c383ebd2de7315
fercarozzi <carozzi@ualberta.ca>
2019-03-19 10:11:57 -0400

    Fix the woopsis at SeisMute

--- src/Processing/SeisMute.jl
+++ src/Processing/SeisMute.jl
@@ -17,20 +17,3 @@ function SeisMute(in;offset=[0.],tmute=0.,vmute=1500.,taper=0.1,dt=0.001)
 
 	return out
 end
-
-
-
-function SeisMute(m::ASCIIString,d::ASCIIString,adj;tmute=0.,vmute=1500.,taper=0.1,ntrace=100000,key=["sx"])
-
-	param["group"] = "some"
-	param["ntrace"] = ntrace
-	parameters = Dict(:tmute=>tmute,:vmute=>vmute,:taper=>taper)
-	if (adj==true)
-		SeisProcess(d,m,param)
-		SeisProcess(d,m,[SeisMute],[parameters];key=["imx"])
-	else
-		SeisProcess(m,d,param)
-		SeisProcess(m,d,[SeisMute],[parameters];key=["imx"])
-	end
-
-end
