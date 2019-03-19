fdd665961f359b82dd2676de61a6e08b84fd5936
fercarozzi <carozzi@ualberta.ca>
2019-03-19 11:17:59 -0400

    Fix SeisMute process

--- src/Processing/SeisMute.jl
+++ src/Processing/SeisMute.jl
@@ -31,7 +31,7 @@ function SeisMute(in::String,out::String,parameters;group="gather",key=["imx","i
 	dt = headers[1].d1
 	parameters = Dict(:offset=>offset,:tmute=>tmute,:vmute=>vmute,:taper=>taper,:dt=>dt)
 	#if (adj==true)
-		SeisProcess(in,out,[SeisMute],[parameters];group=group,key=key,ntrace=ntrace)
+		SeisProcessFile(in,out,[SeisMute],[parameters];group=group,key=key,ntrace=ntrace)
 	#else
 	#	SeisProcess(m,d,[SeisMute],[parameters];key=["imx"])
 	#end
