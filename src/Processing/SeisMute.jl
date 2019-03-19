539c0ba34833333a3617ba3229bb6da934525ebc
fercarozzi <carozzi@ualberta.ca>
2019-03-19 11:07:14 -0400

    Add SeisMute process

--- src/Processing/SeisMute.jl
+++ src/Processing/SeisMute.jl
@@ -1,6 +1,8 @@
 function SeisMute(in;offset=[0.],tmute=0.,vmute=1500.,taper=0.1,dt=0.001)
 
-	nt,nx = size(in)
+	vec= size(d)
+	in = reshape(d,vec[1],:)
+	nx = size(in,2)
 	out = copy(in)
 	for it = 1:nt
 		for ix = 1:nx
@@ -14,6 +16,25 @@ function SeisMute(in;offset=[0.],tmute=0.,vmute=1500.,taper=0.1,dt=0.001)
 			end
 		end
 	end
-
+	out = reshape(out,vec)
 	return out
+end
+
+function SeisMute(in::String,out::String,parameters;group="gather",key=["imx","imy"],ntrace=10000)
+	tmute=0.,vmute=1500.,taper=0.1,dt=0.001)
+
+	tmute = get(parameters,:tmute,0.)
+	vmute = get(parameters,:vmute,1500.)
+	taper = get(parameters,:taper,0.1)
+
+	headers = SeisMain.SeisReadHeaders(in);
+	offset = SeisMain.ExtractHeader(headers,"h")
+	dt = offset[1].d1
+	parameters = Dict(:offset=>offset,:tmute=>tmute,:vmute=>vmute,:taper=>taper,:dt=>dt)
+	#if (adj==true)
+		SeisProcess(d,m,[SeisMute],[parameters];group=group,key=key,ntrace=ntrace)
+	#else
+	#	SeisProcess(m,d,[SeisMute],[parameters];key=["imx"])
+	#end
+
 end
